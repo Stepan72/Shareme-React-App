573f2ccfec950ed899b581e7f5f6abbb7bf84609
Stepan72 <st.nim@list.ru>
2023-04-13 15:40:43 +0600

    version 0.96 pin-user implement

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -132,15 +132,26 @@ function Pin({ pin }) {
                     e.stopPropagation();
                     deletePin(_id);
                   }}
-                  className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outlined-none"
+                  className="bg-white p-2 opacity-70 hover:opacity-100 font-bold text-dark text-base rounded-3xl hover:shadow-md outlined-none"
                 >
-                  Delete
+                  <AiTwotoneDelete />
                 </button>
               )}
             </div>
           </div>
         )}
       </div>
+      <Link
+        to={`/user-profile/${user?._id}`}
+        className="flex gap-2 mt-2 items-center"
+      >
+        <img
+          className="w-8 h-8 rounded-full object-cover"
+          src={postedBy?.image}
+          alt="user-profile"
+        />
+        <p className="font-semibold capitalize">{postedBy?.userName} </p>
+      </Link>
     </div>
   );
 }
